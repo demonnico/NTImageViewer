8e2240bbb00083caaa1b47d9e9db18f677b1e8d5
Nicholas Tau <demon42111915@gmail.com>
2014-03-27 19:58:33 +0800

    Initial Commit

---
+++ NTImageViewer/NTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NTAppDelegate.h
+//  NTImageViewer
+//
+//  Created by Nicholas Tau on 3/27/14.
+//  Copyright (c) 2014 Nicholas Tau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NTImageViewer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NTImageViewer
+//
+//  Created by Nicholas Tau on 3/27/14.
+//  Copyright (c) 2014 Nicholas Tau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NTAppDelegate class]));
+    }
+}
